886fac73393de8602a97d56b5a087e62a75bb7ca
xogh8755 <xogh8755@naver.com>
2023-01-29 20:22:46 +0900

    23-01-28 dont complete detail page

--- Movie App/movie-app/src/routes/Detail.js
+++ Movie App/movie-app/src/routes/Detail.js
@@ -1,15 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import MovieDetail from "../componentes/MovieDetail";
 function Detail() {
   const { id } = useParams();
-  const getMovie = async()=>{
-    const json = await(
-        fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-      ).json;
-  }
+  const [loading, setLoading] = useState(true);
+  const [movieDetail, setMovieDetail] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovieDetail(json.data.movie);
+    setLoading(false);
+  };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+  console.log(movieDetail);
+  return (
+    <div>
+      {loading ? (
+        <h2>Loading Movie Detail ...</h2>
+      ) : (
+        <MovieDetail
+          coverImage={movieDetail.medium_cover_image}
+          title={movieDetail.title}
+          genres={movieDetail.genres}
+          summary={movieDetail.description_full}
+        />
+      )}
+    </div>
+  );
 }
 export default Detail;
